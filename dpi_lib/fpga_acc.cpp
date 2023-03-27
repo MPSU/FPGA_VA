241e5ab8c740275b7a70721f8cef5167b934d3b6
Andrei Solodovnikov <voultboy@yandex.ru>
2023-03-27 12:51:26 +0300

    Fix types in fpga_acc and related

--- dpi_lib/fpga_acc.cpp
+++ dpi_lib/fpga_acc.cpp
@@ -1,18 +1,16 @@
 #include "fpga_acc.h"
 
-extern std::unique_ptr<uint8_t[]> write_buffer, read_buffer;
-extern std::unique_ptr<int> read_fd, write_fd;
+extern std::unique_ptr<uint8_t[], decltype(&delete_aligned)> write_buffer, read_buffer;
+extern std::unique_ptr<int, decltype(&close_pcie_node)> read_fd, write_fd;
 
-extern "C" void get_fpga_acc( const svOpenArrayHandle sig_in,
-                              const unsigned long long arr_size_in,
-                              const unsigned long long arr_size_out,
-                                    svOpenArrayHandle sig_out)
+extern "C" void get_fpga_acc(const svBitVecVal *sig_in,
+                             const unsigned long long arr_size_in,
+                             const unsigned long long arr_size_out,
+                             svBitVecVal *sig_out)
 {
   using namespace std;
-  memcpy(write_buffer.get(), svGetArrayPtr(sig_in), arr_size_in);
-
-  streaming_read(*read_fd, read_buffer.get(), arr_size_in);
+  memcpy(write_buffer.get(), sig_in, arr_size_in);
   streaming_write(*write_fd, write_buffer.get(), arr_size_out);
-
-  memcpy(svGetArrayPtr(sig_out), read_buffer.get(), arr_size_out);
+  streaming_read(*read_fd, read_buffer.get(), arr_size_in);
+  memcpy(sig_out, read_buffer.get(), arr_size_out);
 }
--- dpi_lib/fpga_acc.h
+++ dpi_lib/fpga_acc.h
@@ -1,12 +1,25 @@
 #pragma once
-#include "svdpi.h"
+#include "svdpi.h"  // <- DPI header
 #include <memory>   // <- smart_pointers
 #include <cstring>  // <- memcpy
+#include <iostream> // <- cout
 
-extern "C" void get_fpga_acc(const svOpenArrayHandle sig_in,
-                             const unsigned long long arr_size_in,
-                             const unsigned long long arr_size_out,
-                             svOpenArrayHandle sig_out);
+/*
+From IEEE 1800-2012:
+
+(a chunk of) packed bit array
+typedef uint32_t svBitVecVal
+
+*/
+
+extern "C" void
+get_fpga_acc(const svBitVecVal *sig_in,
+             const unsigned long long arr_size_in,
+             const unsigned long long arr_size_out,
+             svBitVecVal *sig_out);
+
+void delete_aligned(uint8_t *data);
+void close_pcie_node(int *fd);
 
 ssize_t streaming_read(const int fpga_fd, uint8_t *const buffer, const size_t size);
 ssize_t streaming_write(const int fpga_fd, uint8_t *const buffer, const size_t size);
