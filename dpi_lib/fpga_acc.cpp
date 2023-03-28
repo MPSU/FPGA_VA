0f4d60d70de591fc6d604b7b474bd84068355b40
Andrei Solodovnikov <voultboy@yandex.ru>
2023-03-28 19:23:49 +0300

    Добавление профилирования

--- dpi_lib/fpga_acc.cpp
+++ dpi_lib/fpga_acc.cpp
@@ -1,9 +1,24 @@
 #include "fpga_acc.h"
 
+std::chrono::_V2::steady_clock::time_point start;
+
 extern std::unique_ptr<uint8_t[], decltype(&delete_aligned)> write_buffer, read_buffer;
 extern std::unique_ptr<int, decltype(&close_pcie_node)> read_fd, write_fd;
 
-extern "C" void get_fpga_acc(const svBitVecVal *sig_in,
+extern "C"{
+
+void init_timer()
+{
+  start = std::chrono::steady_clock::now();
+}
+
+void print_time(const unsigned long long cntr)
+{
+  auto end = std::chrono::steady_clock::now();
+  std::cout << cntr/ 1e3 / std::chrono::duration<double>(end - start).count() << " kHz";
+}
+
+void get_fpga_acc(const svBitVecVal *sig_in,
                              const unsigned long long arr_size_in,
                              const unsigned long long arr_size_out,
                              svBitVecVal *sig_out)
@@ -13,4 +28,6 @@ extern "C" void get_fpga_acc(const svBitVecVal *sig_in,
   streaming_write(*write_fd, write_buffer.get(), arr_size_out);
   streaming_read(*read_fd, read_buffer.get(), arr_size_in);
   memcpy(sig_out, read_buffer.get(), arr_size_out);
+}
+
 }
--- dpi_lib/fpga_acc.h
+++ dpi_lib/fpga_acc.h
@@ -3,7 +3,7 @@
 #include <memory>   // <- smart_pointers
 #include <cstring>  // <- memcpy
 #include <iostream> // <- cout
-
+#include <chrono>   // <- profiling
 /*
 From IEEE 1800-2012:
 
@@ -12,12 +12,14 @@ typedef uint32_t svBitVecVal
 
 */
 
-extern "C" void
-get_fpga_acc(const svBitVecVal *sig_in,
-             const unsigned long long arr_size_in,
-             const unsigned long long arr_size_out,
-             svBitVecVal *sig_out);
-
+extern "C" {
+  void init_timer();
+  void print_time(const unsigned long long cntr);
+  void get_fpga_acc(const svBitVecVal *sig_in,
+                    const unsigned long long arr_size_in,
+                    const unsigned long long arr_size_out,
+                    svBitVecVal *sig_out);
+}
 void delete_aligned(uint8_t *data);
 void close_pcie_node(int *fd);
 
