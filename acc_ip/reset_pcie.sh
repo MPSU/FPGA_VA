78638a1e8554e065e3b4706a6dac0cb7696bbf07
Evgeniy Primakov <conf@confusion.su>
2023-07-31 17:32:07 +0300

    Add pcie reset script

---
+++ acc_ip/reset_pcie.sh
@@ -0,0 +1,17 @@
+echo "Unload XDMA module"
+rmmod /tools/xdma/xdma.ko
+sleep 1
+#echo "reset pcie device"
+#echo '1' > /sys/bus/pci/devices/0000:01:00.0/reset
+#sleep 1
+echo "remove pcie device"
+echo '1' > /sys/bus/pci/devices/0000:01:00.0/remove
+sleep 1
+echo "rescan pcie bus"
+echo '1' > /sys/bus/pci/rescan
+sleep 1
+echo "load XDMA module"
+/tools/xdma/load_driver.sh
+sleep 1
+echo "set read access to xdma"
+chmod 666 /dev/xdma0_*
